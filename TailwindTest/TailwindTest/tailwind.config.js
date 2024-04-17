96d311a5a8fc0649d2529d5e141f668244ba01cb
Łukasz Krzywizna <lukasz.krzywizna@gmail.com>
2024-04-17 12:03:39 +0200

    Initial commit

---
+++ TailwindTest/TailwindTest/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: "jit",
+    content: [
+        "./index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: []
+}
